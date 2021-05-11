15089c78ab348e50ebe24334e68236c38275dbb7
Kalitosha <kalitoshka@ya.ru>
2021-05-11 16:28:02 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,9 @@
-//const webpack = require('webpack');
-const webpackCli = require('webpack-cli');
 const path = require('path');
 const toCss = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-//const PROD = JSON.parse(process.env.mode || '0');
-//const PROD = webpack;
-
-//console.log(`-------------------------------${webpackCli.options}-----------------------------`)
 
 module.exports = {
-  //mode: 'development',
-  //entry: './src/app.js',
   entry: {
     "bundle": "./src/app.js",
     "bundle.min": "./src/app.js",
@@ -39,13 +31,6 @@ module.exports = {
 
     ]
   },
-  
-  /*optimization: {
-    minimize: true,
-    minimizer: [new UglifyJsPlugin({
-      include: /\.min\.js$/
-    })]
-  },*/
   optimization: {
     minimizer: [
       new OptimizeCssAssetsPlugin({}),
@@ -56,8 +41,7 @@ module.exports = {
   },
   plugins: [
     new toCss({
-      filename: 'css/style.css',
-      //filename: PROD ? 'css/style.min.css' : 'css/style.css',
+      filename: 'css/style.css'
     }),
   ],
   devServer: {
